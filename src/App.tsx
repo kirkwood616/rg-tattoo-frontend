d3a16986816d1290ce2b394060b30bb480e8e4e5
kirkwood616 <kirkwood616@gmail.com>
2022-03-11 16:38:18 -0500

    Setting up initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Main from "./components/Main";
+import RequestAppointment from "./components/RequestAppointment";
 
 function App() {
   return (
     <div className="App">
-      <>APP</>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/request-appointment" element={<RequestAppointment />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
