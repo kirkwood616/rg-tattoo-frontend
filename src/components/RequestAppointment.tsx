e51a24c1770db45d653e26f53946c1264f293b08
kirkwood616 <kirkwood616@gmail.com>
2022-03-11 18:44:35 -0500

    Setting up email validation & onSubmit

--- src/components/RequestAppointment.tsx
+++ src/components/RequestAppointment.tsx
@@ -3,7 +3,8 @@ import Page from "./Page";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { format } from "date-fns";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import AppointmentRequest from "../models/AppointmentRequest";
 
 function RequestAppointment() {
   // STATES FOR DATES
@@ -11,11 +12,11 @@ function RequestAppointment() {
   const [excludedDates, setExcludedDates] = useState<Date[]>([]);
 
   // STATES FOR FORM
-  const [startDate, setStartDate] = useState<Date>();
+  const [startDate, setStartDate] = useState<Date | undefined>();
   const [appointmentTime, setAppointmentTime] = useState();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState<Number>();
+  const [age, setAge] = useState<number>(18);
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [tattooStyle, setTattooStyle] = useState("");
@@ -25,16 +26,68 @@ function RequestAppointment() {
   const [tattooDescription, setTattooDescription] = useState("");
   const [ofAgeConfirm, setOfAgeConfirm] = useState(false);
 
+  const [emailError, setEmailError] = useState(false);
+  console.log(emailError);
+
   // FUNCTIONS
   function disableSundayMonday(date: Date) {
     return date.getDay() !== 0 && date.getDay() !== 1;
+  }
+
+  function emailValidation() {
+    const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    if (!email || regexp.test(email) === false) {
+      setEmailError(true);
+      return false;
+    }
+    setEmailError(false);
+    return true;
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (emailValidation() === false) {
+      console.log("ERROR");
+      return;
+    }
+    let newRequest: AppointmentRequest = {
+      requestSubmittedDate: new Date(),
+      requestDateTime: appointmentTime!,
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+      email: email.toLowerCase(),
+      phoneNumber: phoneNumber,
+      tattooStyle: tattooStyle,
+      tattooPlacement: tattooPlacement,
+      referencePhotoPath: referencePhotoPath,
+      placementPhotoPath: placementPhotoPath,
+      tattooDescription: tattooDescription,
+      isRequestApproved: false,
+      isRequestDenied: false,
+    };
+    console.log(newRequest);
+    setStartDate(undefined);
+    setAppointmentTime(undefined);
+    setFirstName("");
+    setLastName("");
+    setAge(18);
+    setEmail("");
+    setPhoneNumber("");
+    setTattooStyle("");
+    setTattooPlacement("");
+    setReferencePhotoPath("");
+    setPlacementPhotoPath("");
+    setTattooDescription("");
+    setOfAgeConfirm(false);
+    setEmailError(false);
   }
 
   // RENDER
   return (
     <Page title="Request Appointment">
       <div className="RequestAppointment">
-        <form>
+        <form onSubmit={handleSubmit}>
           <span className="label">
             <label htmlFor="datePicker">Select Date:</label>
           </span>
@@ -79,7 +132,7 @@ function RequestAppointment() {
           <span className="label">
             <label htmlFor="email">Email:</label>
           </span>
-          <input type="email" name="email" id="email" pattern=".+@globex\.com" onChange={(e) => setEmail(e.target.value)} required />
+          <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} required />
 
           <span className="label">
             <label htmlFor="tel">Phone:</label>
@@ -89,7 +142,10 @@ function RequestAppointment() {
           <span className="label">
             <label htmlFor="tattooStyle">Tattoo Style:</label>
           </span>
-          <select name="tattooStyle" id="tattooStyle" onChange={(e) => setTattooStyle(e.target.value)}>
+          <select name="tattooStyle" id="tattooStyle" defaultValue={"select"} onChange={(e) => setTattooStyle(e.target.value)} required>
+            <option value="select" disabled>
+              --- Select Style ---
+            </option>
             <option value="Linework">Linework</option>
             <option value="Black & White">Black & White</option>
             <option value="Full Color">Full Color</option>
