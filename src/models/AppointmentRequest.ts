9bc98c2b2a985c14dac605c06ebd1a45a7264cff
kirkwood616 <kirkwood616@gmail.com>
2022-03-11 18:43:22 -0500

    Added AppointmentRequest model

---
+++ src/models/AppointmentRequest.ts
@@ -0,0 +1,19 @@
+interface AppointmentRequest {
+  _id?: string;
+  requestSubmittedDate: Date;
+  requestDateTime: Date;
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  phoneNumber: string;
+  tattooStyle: string;
+  tattooPlacement: string;
+  referencePhotoPath: string;
+  placementPhotoPath: string;
+  tattooDescription: string;
+  isRequestApproved: Boolean;
+  isRequestDenied: Boolean;
+}
+
+export default AppointmentRequest;
