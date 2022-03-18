0a22845c96bf3c51bbbd3e81528f662edf92fb10
kirkwood616 <kirkwood616@gmail.com>
2022-03-18 14:16:19 -0400

    Changed title, return section wrapper for animation

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
+import "./Page.css";
 
 function Page({ children, title }: any) {
   useEffect(() => {
-    document.title = `${title} | RG`;
+    document.title = `${title} | Rack Ruin`;
     window.scrollTo(0, 0);
   }, [title]);
-  return children;
+  return <section className="section">{children}</section>;
 }
 
 export default Page;
