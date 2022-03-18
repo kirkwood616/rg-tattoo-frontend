59beb8453bb62d56a9cb8f4d1a96b000995cfff5
kirkwood616 <kirkwood616@gmail.com>
2022-03-18 14:12:52 -0400

    Added Request Submitted route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import RequestAppointment from "./components/RequestAppointment";
+import RequestSubmitted from "./components/RequestSubmitted";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/request-appointment" element={<RequestAppointment />} />
+          <Route path="/request-submitted" element={<RequestSubmitted />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
