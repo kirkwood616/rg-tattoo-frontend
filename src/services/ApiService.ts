79463d976b30a1a0a815bd946b92c1582889af24
kirkwood616 <kirkwood616@gmail.com>
2022-03-21 17:11:33 -0400

    Available Appointments fetch

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
 import AppointmentRequest from "../models/AppointmentRequest";
+import AvailableAppointments from "../models/AvailableAppointments";
 
-const apiLocalAppointmentRequests = process.env.REACT_APP_API_REQUEST_ROUTE_LOCAL || "";
+const apiRequestAppointment = process.env.REACT_APP_API_REQUEST_ROUTE_LOCAL || "";
+const apiAvailableAppointments = process.env.REACT_APP_API_AVAILABLE_ROUTE_LOCAL || "";
 
 //// APPOINTMENT REQUESTS
 
 // GET ALL
 export function fetchAppointmentRequests(): Promise<AppointmentRequest[]> {
-  return axios.get(apiLocalAppointmentRequests).then((res) => res.data);
+  return axios.get(apiRequestAppointment).then((res) => res.data);
 }
 
 // POST
 export function postAppointmentRequest(request: AppointmentRequest): Promise<AppointmentRequest> {
-  return axios.post<AppointmentRequest>(apiLocalAppointmentRequests, request).then((res) => res.data);
+  return axios.post<AppointmentRequest>(apiRequestAppointment, request).then((res) => res.data);
+}
+
+//// AVAILABLE APPOINTMENTS
+
+// GET ALL
+export function fetchAvailableAppointments(): Promise<AvailableAppointments[]> {
+  return axios.get(apiAvailableAppointments).then((res) => res.data);
 }
