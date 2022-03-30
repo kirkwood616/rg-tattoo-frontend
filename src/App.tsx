a71a7833c85b2c3dc08372f64fc19097f3e2a217
kirkwood616 <kirkwood616@gmail.com>
2022-03-30 17:09:53 -0400

    Routes for Appointment Requests

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import RequestSubmitted from "./components/RequestSubmitted";
 import SetAvailableAppointments from "./components/admin/SetAvailableAppointments";
 import LogIn from "./components/admin/LogIn";
 import AdminHome from "./components/admin/AdminHome";
+import AppointmentRequests from "./components/admin/AppointmentRequests";
+import AppointmentRequestById from "./components/admin/AppointmentRequestById";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
           <Route path="/request-submitted" element={<RequestSubmitted />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/admin/home" element={<AdminHome />} />
+          <Route path="/admin/appointment-requests" element={<AppointmentRequests />} />
+          <Route path="/admin/appointment-requests/:id" element={<AppointmentRequestById />} />
           <Route path="/admin/set-available-appointments" element={<SetAvailableAppointments />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
