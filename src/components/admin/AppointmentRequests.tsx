b772c15335bf3fd1b4f54f0e6ecc11bfd283fedc
kirkwood616 <kirkwood616@gmail.com>
2022-04-01 17:55:25 -0400

    Ternary for mapping appointmentRequests

--- src/components/admin/AppointmentRequests.tsx
+++ src/components/admin/AppointmentRequests.tsx
@@ -10,28 +10,30 @@ function AppointmentRequests() {
   // CONTEXT
   let { appointmentRequests } = useContext(AppContext);
 
-  console.log(appointmentRequests);
-
   return (
     <AdminPage title="Appointment Requests">
       <div className="AppointmentRequests">
         <h1>Appointment Requests</h1>
-        {appointmentRequests.map((request, index) => (
-          <div className="request_container" key={index}>
-            <Link to={`/admin/appointment-requests/${request._id}`}>
-              <div className="request-info_container">
-                <div className="request-date-time_container">{format(new Date(request.requestDateTime), "M-dd-yyyy @ H:mm a")}</div>
-                <div className="request-submitted_container">
-                  <div className="request-info_container">
-                    {request.firstName} {request.lastName}
+        {appointmentRequests.length ? (
+          appointmentRequests.map((request, index) => (
+            <div className="request_container" key={index}>
+              <Link to={`/admin/appointment-requests/${request._id}`}>
+                <div className="request-info_container">
+                  <div className="request-date-time_container">{format(new Date(request.requestDateTime), "M-dd-yyyy @ h:mm a")}</div>
+                  <div className="request-submitted_container">
+                    <div className="request-info_container">
+                      {request.firstName} {request.lastName}
+                    </div>
+                    <div className="request-info_container">{request.email}</div>
+                    <div className="request-info_container">Submitted: {format(new Date(request.requestSubmittedDate), "M-dd-yyyy @ h:mm a")}</div>
                   </div>
-                  <div className="request-info_container">{request.email}</div>
-                  <div className="request-info_container">Submitted: {format(new Date(request.requestSubmittedDate), "M-dd-yyyy @ H:mm a")}</div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        ))}
+              </Link>
+            </div>
+          ))
+        ) : (
+          <h2>No Requests</h2>
+        )}
       </div>
     </AdminPage>
   );
