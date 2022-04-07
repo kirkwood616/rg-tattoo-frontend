09cb3bf8b39cac2034943ea53023bd0ef6880264
kirkwood616 <kirkwood616@gmail.com>
2022-04-07 10:47:38 -0400

    Format Date String

--- src/functions/Formatting.ts
+++ src/functions/Formatting.ts
@@ -3,3 +3,8 @@ export function formatTime(time: string): string {
   if (Number(time[0]) > 0) return time;
   return time.substring(1);
 }
+
+export function formatDate(date: string): string {
+  if (Number(date[0]) > 0) return date;
+  return date.substring(1);
+}
