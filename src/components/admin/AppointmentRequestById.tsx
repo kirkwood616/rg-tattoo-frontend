7fe2ebf1f55cf4ff66a5292368f09d77cbf41902
kirkwood616 <kirkwood616@gmail.com>
2022-04-07 10:47:05 -0400

    Formatting

--- src/components/admin/AppointmentRequestById.tsx
+++ src/components/admin/AppointmentRequestById.tsx
@@ -98,7 +98,7 @@ function AppointmentRequestById() {
             )}
           </div>
           <div className="request-table-title">Request Submitted</div>
-          <div className="request-table-info">{format(new Date(request!.requestSubmittedDate), "M-dd-yyyy @ H:mm a")}</div>
+          <div className="request-table-info">{format(new Date(request!.requestSubmittedDate), "M/dd/yyyy @ h:mm a")}</div>
         </div>
         <GoButton type="button" text="APPROVE" backgroundColor="green" onClick={() => setIsApproveActive(true)} />
         <GoButton type="button" text="REJECT" backgroundColor="red" onClick={() => setIsRejectActive(true)} />
--- src/components/admin/AppointmentRequests.tsx
+++ src/components/admin/AppointmentRequests.tsx
@@ -1,10 +1,10 @@
-import "./AppointmentRequests.css";
-
-import { format } from "date-fns";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
+import { formatDate, formatTime } from "../../functions/Formatting";
 import AppContext from "../../context/AppContext";
 import AdminPage from "./AdminPage";
-import { Link } from "react-router-dom";
+import "./AppointmentRequests.css";
 
 function AppointmentRequests() {
   // CONTEXT
@@ -20,14 +20,14 @@ function AppointmentRequests() {
               <Link to={`/admin/appointment-requests/${request._id}`}>
                 <div className="request-info_container">
                   <div className="request-date-time_container">
-                    {request.requestDate} @ {request.requestTime}
+                    {formatDate(request.requestDate)} @ {formatTime(request.requestTime)}
                   </div>
                   <div className="request-submitted_container">
                     <div className="request-info_container">
                       {request.firstName} {request.lastName}
                     </div>
                     <div className="request-info_container">{request.email}</div>
-                    <div className="request-info_container">Submitted: {format(new Date(request.requestSubmittedDate), "M-dd-yyyy @ h:mm a")}</div>
+                    <div className="request-info_container">Submitted: {format(new Date(request.requestSubmittedDate), "M/dd/yyyy @ h:mm a")}</div>
                   </div>
                 </div>
               </Link>
