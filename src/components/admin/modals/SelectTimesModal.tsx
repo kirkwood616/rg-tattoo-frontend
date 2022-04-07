e599122a6af6c21b2a1c80a4d589e02806abaae3
kirkwood616 <kirkwood616@gmail.com>
2022-04-07 09:08:59 -0400

    Modals for admin

---
+++ src/components/admin/modals/SelectTimesModal.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction } from "react";
+import { timePickerValues } from "../../../admin/AdminSettings";
+import { formatTime } from "../../../functions/Formatting";
+import ModalWindow from "../../modals/ModalWindow";
+import "./SelectTimesModal.css";
+
+interface Props {
+  isTimesActive: boolean;
+  setIsTimesActive: Dispatch<SetStateAction<boolean>>;
+  setStartTime: Dispatch<SetStateAction<string>>;
+}
+
+function SelectTimesModal({ isTimesActive, setIsTimesActive, setStartTime }: Props) {
+  function onTimeClick(e: React.MouseEvent<HTMLOptionElement, MouseEvent>) {
+    setStartTime(e.currentTarget.value);
+    setIsTimesActive(false);
+  }
+  return (
+    <ModalWindow isActive={isTimesActive} setIsActive={setIsTimesActive} className="time-select_container">
+      {timePickerValues!.map((time, index) => (
+        <option value={time} key={index} className="time-option" onClick={(e) => onTimeClick(e)}>
+          {formatTime(time)}
+        </option>
+      ))}
+    </ModalWindow>
+  );
+}
+
+export default SelectTimesModal;
