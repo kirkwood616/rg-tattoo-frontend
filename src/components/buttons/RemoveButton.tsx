c9551a093b12bbc815586a13baedf2308fb4393c
kirkwood616 <kirkwood616@gmail.com>
2022-04-21 16:52:50 -0400

    Remove Button SVG

--- src/components/buttons/RemoveButton.tsx
+++ src/components/buttons/RemoveButton.tsx
@@ -7,9 +7,13 @@ interface Props {
 
 function RemoveButton({ index, onClick }: Props) {
   return (
-    <button type="button" className="remove-button" onClick={() => onClick(index)}>
-      X
-    </button>
+    <svg className="close-svg" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 500 500" onClick={() => onClick(index)}>
+      <path
+        d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M327.115,365.904
+			L256,294.789l-71.115,71.115l-38.789-38.789L217.211,256l-71.115-71.115l38.789-38.789L256,217.211l71.115-71.115l38.789,38.789
+			L294.789,256l71.115,71.115L327.115,365.904z"
+      />
+    </svg>
   );
 }
 
