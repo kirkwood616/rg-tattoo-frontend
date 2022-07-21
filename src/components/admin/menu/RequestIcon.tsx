74a89805a07f1eed0083d4d2264ce4e17437db35
kirkwood616 <kirkwood616@gmail.com>
2022-07-21 19:11:08 -0400

    Refactor, AdminContext configuring

--- src/components/admin/menu/RequestIcon.tsx
+++ src/components/admin/menu/RequestIcon.tsx
@@ -4,12 +4,12 @@ import "./RequestIcon.css";
 
 function RequestIcon() {
   // CONTEXT
-  let { appointmentRequests } = useContext(AdminContext);
+  let { newAppointmentRequests } = useContext(AdminContext);
 
   return (
     <div className="RequestIcon">
       <div className="request-number_container">
-        <span className="request-number">{appointmentRequests.length}</span>
+        <span className="request-number">{newAppointmentRequests.length}</span>
       </div>
     </div>
   );
