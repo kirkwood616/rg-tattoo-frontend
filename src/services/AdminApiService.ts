33564ed6d567e522260e4ee8bd139c6987c26420
kirkwood616 <kirkwood616@gmail.com>
2022-08-29 12:40:11 -0400

    Added admin interceptor & refactor

--- src/services/AdminApiService.ts
+++ src/services/AdminApiService.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { getDownloadURL, ref, StorageReference } from "firebase/storage";
-import { auth, storage } from "../firebaseConfig";
+import { storage } from "../firebaseConfig";
 import { AppointmentRequest } from "../models/AppointmentRequest";
 import AvailableAppointments from "../models/AvailableAppointments";
+import admin from "./AdminInterceptor";
 
 const apiRequestAppointment = process.env.REACT_APP_API_REQUEST_ROUTE_LOCAL || "";
-const apiAvailableAppointments = process.env.REACT_APP_API_AVAILABLE_ROUTE_LOCAL || "";
 const apiDeniedAppointments = process.env.REACT_APP_API_DENIED_ROUTE_LOCAL || "";
-const apiBaseRoute = process.env.REACT_APP_API_BASE_ROUTE || "";
 
 //// 1. APPOINTMENT REQUESTS
 // GET ALL REQUESTS
@@ -31,64 +30,54 @@ export function fetchDeniedRequests(): Promise<AppointmentRequest[]> {
   return axios.get(apiDeniedAppointments).then((res) => res.data);
 }
 
-//// 2. AVAILABLE APPOINTMENTS
+// SWR
+
+//// 1. AVAILABLE APPOINTMENTS
 // POST
-export function postAvailableAppointment(appointment: AvailableAppointments): Promise<AvailableAppointments> {
-  return axios.post(apiAvailableAppointments, appointment).then((res) => res.data);
+export async function postAvailableAppointment(appointment: AvailableAppointments): Promise<AvailableAppointments> {
+  const res = await admin.post("/available-appointments/create", appointment);
+  return res.data;
 }
 
 // PUT
-export function updateAvailableAppointment(id: string, appointmentDateTimes: AvailableAppointments): Promise<AvailableAppointments> {
-  return axios.put(`${apiAvailableAppointments}/${id}`, appointmentDateTimes).then((res) => res.data);
+export async function updateAvailableAppointment(id: string, appointmentDateTimes: AvailableAppointments): Promise<AvailableAppointments> {
+  const res = await admin.put(`/available-appointments/update/${id}`, appointmentDateTimes);
+  return res.data;
 }
 
-// SWR
+//// 2. APPOINTMENT REQUESTS
 export async function getRequests(url: string): Promise<AppointmentRequest[] | void> {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.get(`${apiBaseRoute}/${url}`, { headers: { token } });
+  const res = await admin.get(url);
   return res.data;
 }
 
 export async function getRequest(url: string): Promise<AppointmentRequest | void> {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.get(`${apiBaseRoute}/${url}`, { headers: { token } });
+  const res = await admin.get(url);
   return res.data;
 }
 
 export async function approveNewRequest(request: AppointmentRequest) {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.put(`${apiBaseRoute}/appointment-requests/new/approve/${request._id}`, request, { headers: { token } });
+  const res = await admin.put(`appointment-requests/new/approve/${request._id}`, request);
   return res.data;
 }
 
 export async function sendDepositReceivedRequest(request: AppointmentRequest) {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.put(`${apiBaseRoute}/appointment-requests/awaiting-deposit/received/${request._id}`, request, { headers: { token } });
+  const res = await admin.put(`appointment-requests/awaiting-deposit/received/${request._id}`, request);
   return res.data;
 }
 
 export async function sendCompletedRequest(request: AppointmentRequest) {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.put(`${apiBaseRoute}/appointment-requests/deposit-received/completed/${request._id}`, request, { headers: { token } });
+  const res = await admin.put(`appointment-requests/deposit-received/completed/${request._id}`, request);
   return res.data;
 }
 
 export async function sendCanceledRequest(request: AppointmentRequest) {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.put(`${apiBaseRoute}/appointment-requests/cancel-request/${request._id}`, request, { headers: { token } });
+  const res = await admin.put(`appointment-requests/cancel-request/${request._id}`, request);
   return res.data;
 }
 
 export async function putClosedRequest(request: AppointmentRequest) {
-  const token = await auth.currentUser?.getIdToken(true);
-  if (!token) return;
-  const res = await axios.put(`${apiBaseRoute}/archive/update/${request._id}`, request, { headers: { token } });
+  const res = await admin.put(`archive/update/${request._id}`, request);
   return res.data;
 }
 
