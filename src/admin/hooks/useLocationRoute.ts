70c7c3bf60001984456ca4a2b67ad7ef468a2b95
kirkwood616 <kirkwood616@gmail.com>
2022-09-01 11:43:54 -0400

    Added isAdmin and var types

--- src/admin/hooks/useLocationRoute.ts
+++ src/admin/hooks/useLocationRoute.ts
@@ -1,13 +1,14 @@
-import { RequestStatus } from "models/AppointmentRequest";
-import { useLocation, useParams } from "react-router-dom";
+import { Location, Params, useLocation, useParams } from "react-router-dom";
 import { formatTitle } from "utils/Formatting";
 
 export default function useLocationRoute() {
-  const location = useLocation();
-  const params = useParams();
-  const route: RequestStatus = params.route as RequestStatus;
-  const id = params.id;
+  const location: Location = useLocation();
+  const pathname: string = location.pathname;
+  const params: Readonly<Params<string>> = useParams();
+  const route: string | undefined = params.route;
+  const id: string | undefined = params.id;
   const title: string = formatTitle(route);
+  const isAdmin: boolean = pathname.includes("admin");
 
-  return { location, params, route, id, title };
+  return { location, pathname, params, route, id, title, isAdmin };
 }
