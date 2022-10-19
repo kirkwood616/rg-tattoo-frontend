666498c392241152ab91fb90febc69d4a29ccbd3
kirkwood616 <kirkwood616@gmail.com>
2022-10-18 23:10:22 -0400

    SelectList (re-usable)

---
+++ src/components/modals/SelectList.tsx
@@ -0,0 +1,38 @@
+import RequestContext from "context/RequestContext";
+import { Dispatch, SetStateAction, useContext } from "react";
+import ModalWindow from "./ModalWindow";
+import "./SelectList.css";
+
+type SelectAction = "budget" | "tattooStyle" | "appointmentTime";
+
+interface Props {
+  isSelectActive: boolean;
+  setIsSelectActive: Dispatch<SetStateAction<boolean>>;
+  selectList: string[];
+  actionType?: SelectAction;
+  selectFunction?: (arg: string) => void;
+  formatter?: (arg: string) => string;
+}
+
+function SelectList({ isSelectActive, setIsSelectActive, selectList, actionType, selectFunction, formatter }: Props) {
+  // CONTEXT
+  const { dispatch } = useContext(RequestContext);
+
+  // FUNCTION
+  function onSelectClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if (actionType) dispatch({ type: actionType, value: e.currentTarget.value });
+    if (selectFunction) selectFunction(e.currentTarget.value);
+    setIsSelectActive((current) => !current);
+  }
+  return (
+    <ModalWindow isActive={isSelectActive} setIsActive={setIsSelectActive} className="select_container">
+      {selectList.map((item, index) => (
+        <button value={item} key={item + index} className="select-option" onClick={(e) => onSelectClick(e)}>
+          {formatter ? formatter(item) : item}
+        </button>
+      ))}
+    </ModalWindow>
+  );
+}
+
+export default SelectList;
