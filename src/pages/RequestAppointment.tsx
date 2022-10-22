684ea7371f909e98b61229bddd67a935ba1b11a4
kirkwood616 <kirkwood616@gmail.com>
2022-10-22 16:10:13 -0400

    Refactored submit, button & added toggleLoading

--- src/pages/RequestAppointment.tsx
+++ src/pages/RequestAppointment.tsx
@@ -17,19 +17,14 @@ import { generateNewRequest } from "utils/Request";
 import "./RequestAppointment.css";
 
 function RequestAppointment() {
-  // CONTEXT
-  const { setIsLoading } = useContext(AppContext);
+  const { toggleLoading } = useContext(AppContext);
   const { setAvailableAppointmentsTimes, state, dispatch } = useContext(RequestContext);
-
-  // STATE
   const [isSubmitActive, setIsSubmitActive] = useState(false);
 
-  // SWR
   const { data: available, error } = useSWR("/available-appointments", getAvailableAppointments, {
     revalidateOnFocus: false,
   });
 
-  // NAVIGATE
   const navigate = useNavigate();
 
   // CHECK FOR DATE IN DATABASE
@@ -49,45 +44,43 @@ function RequestAppointment() {
     }
   }, [state.startDate.value, available, setAvailableAppointmentsTimes]);
 
-  // HANDLE SUBMIT
   function onSubmit(e: FormEvent): void {
     e.preventDefault();
-    setIsSubmitActive((current) => !current);
-  }
-
-  function handleSubmit(): void {
     dispatch({ type: "submitCount" });
     if (state.hasErrors) {
       console.log("ERRORS");
       return;
     } else {
-      const newRequest = generateNewRequest(state);
-      setIsLoading((current) => !current);
-      postAppointmentRequest(newRequest)
-        .then(() => {
-          handlePhotoUpload(state, "reference");
-          if (state.placementPhoto.value) handlePhotoUpload(state, "placement");
-        })
-        .catch((error) => {
-          console.error(error);
-          setIsLoading((current) => !current);
-          navigate("/");
-        })
-        .then(() => {
-          setIsLoading((current) => !current);
-          navigate("/request-submitted");
-        });
+      setIsSubmitActive((current) => !current);
     }
   }
 
-  // RENDER
+  function handleSubmit(): void {
+    const newRequest = generateNewRequest(state);
+    toggleLoading();
+    postAppointmentRequest(newRequest)
+      .then(() => {
+        handlePhotoUpload(state, "reference");
+        if (state.placementPhoto.value) handlePhotoUpload(state, "placement");
+      })
+      .catch((error) => {
+        console.error(error);
+        toggleLoading();
+        navigate("/");
+      })
+      .then(() => {
+        toggleLoading();
+        navigate("/request-submitted");
+      });
+  }
+
   if (error) return <h1>Something went wrong!</h1>;
   if (!available) return <LoadingDotsIcon />;
   return (
     <Page title="Request Appointment">
       <div className="RequestAppointment">
         <h1>Request Appointment</h1>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={(e) => onSubmit(e)}>
           <Field.SelectDate />
           {state.startDate.value && <Field.AppointmentTimes />}
           {state.appointmentTime.value && (
@@ -106,11 +99,9 @@ function RequestAppointment() {
               <Field.RequestConfirm />
               {state.appointmentTime.value && (
                 <GoButton
-                  type="button"
-                  text="Submit Request"
-                  backgroundColor="green"
-                  isDisabled={false}
-                  onClick={() => setIsSubmitActive((current) => !current)}
+                  type="submit"
+                  text="SUBMIT REQUEST"
+                  backgroundColor={state.hasErrors ? "var(--dark-gray-3)" : "green"}
                 />
               )}
               {isSubmitActive && (
