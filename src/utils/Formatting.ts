9fd7c6253fe56cfe4ce3bae874cfca0ea6deba82
kirkwood616 <kirkwood616@gmail.com>
2022-10-21 20:20:46 -0400

    File name formatting

--- src/utils/Formatting.ts
+++ src/utils/Formatting.ts
@@ -1,3 +1,6 @@
+import { PhotoType } from "models/AppointmentRequest";
+import { RequestReducer } from "models/RequestReducer";
+
 /**
  * Formats the current browser path & parameters into a title to be displayed
  * within components. If no path or parameters in the route, it will return
@@ -39,7 +42,12 @@ export function formatRouteTitle(title: string | undefined): string {
  * @param date Date object or parsed date-time string. Date-time strings should be parsed to ISO or UTC. Preferred usage of `date.toLocaleString("en-US")` on a `Date` object.
  */
 export function formatUnitedStatesDate(date: Date | string): string {
-  const dateENUS = new Date(date).toLocaleDateString("en-US", { timeZone: "America/New_York", month: "2-digit", day: "2-digit", year: "numeric" });
+  const dateENUS = new Date(date).toLocaleDateString("en-US", {
+    timeZone: "America/New_York",
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  });
   if (dateENUS[0] === "0") {
     return dateENUS.substring(1);
   } else {
@@ -57,7 +65,11 @@ export function formatUnitedStatesDate(date: Date | string): string {
  * @param date Date object or parsed date-time string. Date-time strings should be parsed to ISO or UTC. Preferred usage of `date.toLocaleString("en-US")` on a `Date` object.
  */
 export function formatEstTime(date: Date | string): string {
-  const estDate = new Date(date).toLocaleTimeString("en-US", { timeZone: "America/New_York", hour: "2-digit", minute: "2-digit" });
+  const estDate = new Date(date).toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return estDate;
 }
 
@@ -71,7 +83,11 @@ export function formatEstTime(date: Date | string): string {
  * @param date Date object or parsed date-time string. Date-time strings should be parsed to ISO or UTC. Preferred usage of `date.toLocaleString("en-US")` on a `Date` object.
  */
 export function formatEstTimeWithSuffix(date: Date | string): string {
-  const estDate = new Date(date).toLocaleTimeString("en-US", { timeZone: "America/New_York", hour: "2-digit", minute: "2-digit" });
+  const estDate = new Date(date).toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   if (estDate[0] === "0") {
     return `${estDate.substring(1)} (EST)`;
   } else {
@@ -103,4 +119,40 @@ export function formatTimeNoLeadingZero(time: string): string {
 export function formatDateNoLeadingZero(date: string): string {
   if (Number(date[0]) > 0) return date;
   return date.substring(1);
+}
+
+/**
+ *
+ * @param filePath A filepath `string` from file upload
+ * @returns A `string` with special characters removed & spaces replaced with underscores.
+ * @example 'Bill & Ted.jpg' => 'Bill__Ted.jpg'
+ * @example 'Fl!ck3r!ng .png ' => 'Flck3rng.png'
+ */
+export function formatFileName(filePath: string): string {
+  const extension = filePath
+    .substring(filePath.lastIndexOf(".") + 1)
+    .replace(/[^a-zA-Z 0-9]+/g, "")
+    .replace(" ", "_");
+  const name = filePath.substring(0, filePath.lastIndexOf("."));
+  const newName = name.replace(/[^a-zA-Z 0-9 _-]+/g, "").replace(/\s/g, "_");
+  const formattedFileName = `${newName}.${extension}`;
+  return formattedFileName;
+}
+
+/**
+ *
+ * @param state `RequestReducer` state object
+ * @param photoType `PhotoType` ("reference" | "placement")
+ * @returns A formatted `string` that will be used as the file name in storage & in the Request object.
+ */
+export function formatPhotoPath(state: RequestReducer, photoType: PhotoType) {
+  const prefix = `${state.firstName.value}-${state.lastName.value}`;
+  switch (photoType) {
+    case "reference":
+      if (!state.referencePhoto.value) return "";
+      return `${prefix}-ref-${formatFileName(state.referencePhoto.value.name)}`;
+    case "placement":
+      if (!state.placementPhoto.value) return "";
+      return `${prefix}-place-${formatFileName(state.placementPhoto.value.name)}`;
+  }
 }
