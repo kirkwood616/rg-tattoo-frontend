7b51ef686744e090cf0dd5bdcd1ee5e15bc69975
kirkwood616 <kirkwood616@gmail.com>
2022-10-21 20:19:41 -0400

    Added generateNewRequest, handlePhotoUpload

--- src/pages/RequestAppointment.tsx
+++ src/pages/RequestAppointment.tsx
@@ -5,14 +5,14 @@ import * as Field from "components/request-form-fields";
 import AppContext from "context/AppContext";
 import RequestContext from "context/RequestContext";
 import { format } from "date-fns";
-import { AppointmentRequest } from "models/AppointmentRequest";
 import AvailableAppointments from "models/AvailableAppointments";
 import { FormEvent, useContext, useEffect } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { useNavigate } from "react-router-dom";
 import { getAvailableAppointments, postAppointmentRequest } from "services/ApiService";
 import useSWR from "swr";
-import { handlePlacementPhotoUpload, handleReferencePhotoUpload } from "utils/PhotoUpload";
+import { handlePhotoUpload } from "utils/PhotoUpload";
+import { generateNewRequest } from "utils/Request";
 import "./RequestAppointment.css";
 
 function RequestAppointment() {
@@ -53,43 +53,12 @@ function RequestAppointment() {
       console.log("ERRORS");
       return;
     } else {
-      console.log("SUBMITTED");
-      const newRequest: AppointmentRequest = {
-        requestSubmittedDate: new Date(),
-        requestDate: format(state.startDate.value!, "MM-dd-yyyy"),
-        requestTime: state.appointmentTime.value,
-        firstName: state.firstName.value,
-        lastName: state.lastName.value,
-        age: state.age.value,
-        email: state.email.value.toLowerCase(),
-        phoneNumber: state.phoneNumber.value,
-        tattooStyle: state.tattooStyle.value,
-        tattooPlacement: state.tattooPlacement.value,
-        budget: state.budget.value,
-        referencePhotoPath: state.referencePhoto.value
-          ? `${state.firstName.value}-${state.lastName!.value}-ref-${state.referencePhoto.value.name}`
-          : "",
-        placementPhotoPath: state.placementPhoto.value
-          ? `${state.firstName.value}-${state.lastName!.value}-place-${state.placementPhoto.value.name}`
-          : "",
-        tattooDescription: state.tattooDescription.value,
-        requestConfirm: state.requestConfirm.value,
-        requestStatus: "new",
-        depositAmmountReceived: 0,
-        isRequestClosed: false,
-        priceCharged: 0,
-        historyLog: [
-          {
-            dateCreated: new Date(),
-            action: "New Appointment Request Submitted.",
-          },
-        ],
-      };
+      const newRequest = generateNewRequest(state);
       setIsLoading(true);
       postAppointmentRequest(newRequest)
         .then(() => {
-          handleReferencePhotoUpload(state);
-          if (state.placementPhoto.value) handlePlacementPhotoUpload(state);
+          handlePhotoUpload(state, "reference");
+          if (state.placementPhoto.value) handlePhotoUpload(state, "placement");
         })
         .catch((error) => console.error(error))
         .then(() => {
