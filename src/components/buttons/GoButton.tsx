8bcb4622f497dd12d61a57f7b742b0b88381c55b
kirkwood616 <kirkwood616@gmail.com>
2022-10-22 16:11:33 -0400

    updated onClick props, styling change

--- src/components/buttons/GoButton.tsx
+++ src/components/buttons/GoButton.tsx
@@ -1,16 +1,23 @@
+import { FormEvent } from "react";
 import "./GoButton.css";
 
 interface Props {
   type: "submit" | "reset" | "button";
   text: string;
   backgroundColor: string;
-  onClick?: () => void;
+  onClick?: () => void | Promise<void> | ((e: FormEvent<HTMLFormElement>) => void);
   isDisabled?: boolean;
 }
 
 function GoButton({ type, text, backgroundColor, onClick, isDisabled }: Props): JSX.Element {
   return (
-    <button type={type} onClick={onClick} className="go-button" style={{ backgroundColor: backgroundColor }} disabled={isDisabled}>
+    <button
+      type={type}
+      onClick={onClick}
+      className="go-button"
+      style={{ backgroundColor: backgroundColor }}
+      disabled={isDisabled}
+    >
       {text}
     </button>
   );
