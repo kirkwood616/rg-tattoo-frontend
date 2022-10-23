3564fb241e5660deca7307fb1e4037b63ce8dec6
kirkwood616 <kirkwood616@gmail.com>
2022-10-23 18:05:56 -0400

    Error404 route

---
+++ src/pages/Error404.tsx
@@ -0,0 +1,11 @@
+// import "./Error404.css";
+
+function Error404() {
+  return (
+    <div className="Error404">
+      <h1>Error 404: Not Found</h1>
+    </div>
+  );
+}
+
+export default Error404;
