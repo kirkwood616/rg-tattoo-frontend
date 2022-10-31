75315968b7bbe8e5f628282aa371f84be2038ccb
kirkwood616 <kirkwood616@gmail.com>
2022-10-31 10:22:55 -0400

    styling, refactor

--- src/components/modals/AreYouSure.tsx
+++ src/components/modals/AreYouSure.tsx
@@ -13,9 +13,11 @@ interface Props {
 function AreYouSure({ isActive, setIsActive, yesFunction, yesButtonText }: Props) {
   return (
     <ModalWindow isActive={isActive} setIsActive={setIsActive} className="yes-confirm">
-      <h2>Are You Sure?</h2>
-      <GoButton type="button" text={yesButtonText} backgroundColor="green" onClick={yesFunction} />
-      <GoButton type="button" text="CANCEL" backgroundColor="red" onClick={() => setIsActive((current) => !current)} />
+      <div className="are-you-sure">
+        <h2>Are You Sure?</h2>
+        <GoButton type="button" text={yesButtonText} backgroundColor="green" onClick={yesFunction} />
+        <GoButton type="button" text="CANCEL" backgroundColor="red" onClick={() => setIsActive((current) => !current)} />
+      </div>
     </ModalWindow>
   );
 }
--- src/components/modals/ModalWindow.tsx
+++ src/components/modals/ModalWindow.tsx
@@ -4,14 +4,14 @@ import "./ModalWindow.css";
 interface Props {
   isActive: boolean;
   setIsActive: Dispatch<SetStateAction<boolean>>;
-  className: string;
+  className?: string;
   children: ReactNode;
 }
 
 function ModalWindow({ isActive, setIsActive, className, children }: Props) {
   return (
     <div className={isActive ? "ModalWindow" : "ModalWindow hide"} onClick={() => setIsActive(false)}>
-      <div className={className} onClick={(e) => e.stopPropagation()}>
+      <div className="modal_body" onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
