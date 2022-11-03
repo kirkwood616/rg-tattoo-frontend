2eb2402b0f299bc46896e3588363a775ed029f40
kirkwood616 <kirkwood616@gmail.com>
2022-11-02 20:01:32 -0400

    removed ClosedRequestActions

--- src/admin/features/RequestActions/RequestActions.tsx
+++ src/admin/features/RequestActions/RequestActions.tsx
@@ -1,21 +1,20 @@
 import { AppointmentRequest } from "models/AppointmentRequest";
 import "./RequestActions.css";
-import ClosedRequestActions from "./RequestActionsClosed";
 import OpenRequestActions from "./RequestActionsOpen";
 
 interface Props {
   request: AppointmentRequest;
 }
 
-function ReqActions({ request }: Props) {
+function RequestActions({ request }: Props) {
   switch (request.requestStatus) {
     case "new":
     case "awaiting-deposit":
     case "deposit-received":
       return <OpenRequestActions request={request} />;
     default:
-      return <ClosedRequestActions request={request} />;
+      return <></>;
   }
 }
 
-export default ReqActions;
+export default RequestActions;
