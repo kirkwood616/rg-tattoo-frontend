f0fe7e01fd80f6afed6b8abbc40fa183ea04ab7a
kirkwood616 <kirkwood616@gmail.com>
2022-11-02 20:00:21 -0400

    added denied & canceled cases, updated approved

--- src/admin/utils/RequestApiCall.ts
+++ src/admin/utils/RequestApiCall.ts
@@ -1,15 +1,33 @@
-import { approveNewRequest, sendCompletedRequest, sendDepositReceivedRequest } from "admin/services/AdminApiService";
+import {
+  sendApprovedRequest,
+  sendCanceledRequest,
+  sendCompletedRequest,
+  sendDeniedRequest,
+  sendDepositReceivedRequest,
+} from "admin/services/AdminApiService";
 import { AppointmentRequest } from "models/AppointmentRequest";
 
-// Request is updated to it's next status from actionSubmitRequest()
-// Use next status in chain (e.g. "new" === "awaiting-deposit")
+/**
+ * Conditional function that evaluates an AppointmentRequest object's `requestStatus` property,
+ * which then returns an async API request sending the object to the backend for updating.
+ *
+ * `updatedRequest` needs to have it's `requestStatus` property set to
+ * the correct status **_before_** being passed as a parameter.
+ *
+ * @param updatedRequest AppointmentRequest object
+ * @returns Async API function sending the updated request to the backend.
+ */
 export default function requestApiCall(updatedRequest: AppointmentRequest) {
   switch (updatedRequest.requestStatus) {
     case "awaiting-deposit":
-      return approveNewRequest(updatedRequest);
+      return sendApprovedRequest(updatedRequest);
     case "deposit-received":
       return sendDepositReceivedRequest(updatedRequest);
     case "completed":
       return sendCompletedRequest(updatedRequest);
+    case "denied":
+      return sendDeniedRequest(updatedRequest);
+    case "canceled":
+      return sendCanceledRequest(updatedRequest);
   }
 }
