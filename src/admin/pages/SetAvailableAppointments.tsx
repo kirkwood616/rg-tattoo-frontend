238638197e8d05e2250f4f51143c50125053c51d
kirkwood616 <kirkwood616@gmail.com>
2022-11-10 20:59:37 -0500

    extracted SetAvailableAppointment features

--- src/admin/pages/SetAvailableAppointments.tsx
+++ src/admin/pages/SetAvailableAppointments.tsx
@@ -1,8 +1,8 @@
 import AdminPage from "admin/components/AdminPage";
+import * as SetAvailable from "admin/features/SetAvailableApts";
 import { postAvailableAppointment, updateAvailableAppointment } from "admin/services/AdminApiService";
 import { timePickerValues } from "admin/settings/AdminSettings";
 import GoButton from "components/buttons/GoButton";
-import RemoveButton from "components/buttons/RemoveButton";
 import LoadingDotsIcon from "components/loading/LoadingDotsIcon";
 import AreYouSure from "components/modals/AreYouSure";
 import SelectList from "components/modals/SelectList";
@@ -10,9 +10,7 @@ import AppContext from "context/AppContext";
 import { format } from "date-fns";
 import AvailableAppointments from "models/AvailableAppointments";
 import { useContext, useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { getAvailableAppointments } from "services/ApiService";
 import useSWR, { useSWRConfig } from "swr";
 import { formatTimeNoLeadingZero } from "utils/Formatting";
@@ -26,9 +24,11 @@ function SetAvailableAppointments() {
   const [isSaveActive, setIsSaveActive] = useState<boolean>(false);
 
   const { toggleLoading, toggleModalOpen } = useContext(AppContext);
+
   const { data: available, error: availableError } = useSWR("/available-appointments", getAvailableAppointments, {
     revalidateOnFocus: false,
   });
+
   const { mutate } = useSWRConfig();
 
   // CHECK FOR DATE IN DATABASE
@@ -106,54 +106,15 @@ function SetAvailableAppointments() {
         <div className="SetAvailableAppointments">
           <h1>Set Available Appointments</h1>
 
-          <div className="date-picker_container">
-            <span className="label">
-              <label htmlFor="date-picker">Date:</label>
-            </span>
-            <DatePicker
-              name="date-picker"
-              id="date-picker"
-              withPortal
-              selected={startDate}
-              minDate={new Date()}
-              onChange={(date: Date) => setStartDate(date)}
-            />
-          </div>
+          <SetAvailable.DateSelect startDate={startDate} setStartDate={setStartDate} />
 
-          <div className="button-container">
-            <GoButton
-              type="button"
-              text="ADD ALL TIMES"
-              backgroundColor="var(--dark-gray-1)"
-              onClick={() => setAppointmentTimes(timePickerValues!)}
-            />
-            <GoButton
-              type="button"
-              text="REMOVE ALL TIMES"
-              backgroundColor="var(--dark-gray-1)"
-              onClick={() => setAppointmentTimes([])}
-            />
-          </div>
+          <SetAvailable.Controls setAppointmentTimes={setAppointmentTimes} setIsTimesActive={setIsTimesActive} />
 
-          <GoButton type="button" text="ADD TIME" onClick={() => toggleModalOpen(setIsTimesActive)} />
-
-          <TransitionGroup className="available-times_container">
-            {appointmentTimes.length > 0 &&
-              appointmentTimes.map((time, index) => (
-                <CSSTransition key={time} timeout={250} classNames="time">
-                  <div className="time">
-                    <span className="available-time">{formatTimeNoLeadingZero(time)}</span>
-                    <RemoveButton onClick={() => removeTime(index)} />
-                  </div>
-                </CSSTransition>
-              ))}
-          </TransitionGroup>
-
-          <CSSTransition in={!appointmentTimes.length} unmountOnExit timeout={50} classNames="no-times-set">
-            <div className="no-times-set" onClick={() => toggleModalOpen(setIsTimesActive)}>
-              NO TIMES SET
-            </div>
-          </CSSTransition>
+          <SetAvailable.SelectedTimes
+            appointmentTimes={appointmentTimes}
+            removeTime={removeTime}
+            setIsTimesActive={setIsTimesActive}
+          />
         </div>
 
         {isTimesActive && (
