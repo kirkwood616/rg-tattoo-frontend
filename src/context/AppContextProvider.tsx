50f705ad8f124c482d7dced8b30f756e406f76a5
kirkwood616 <kirkwood616@gmail.com>
2022-11-18 00:16:07 -0500

    removed bg lock, added outside click close

--- src/context/AppContextProvider.tsx
+++ src/context/AppContextProvider.tsx
@@ -1,5 +1,5 @@
 import { User } from "firebase/auth";
-import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import AppContext from "./AppContext";
 
 interface Props {
@@ -10,14 +10,6 @@ export default function AppContextProvider({ children }: Props) {
   const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    if (isModalOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  }, [isModalOpen]);
 
   function toggleModalOpen(setIsActive?: Dispatch<SetStateAction<boolean>>) {
     if (setIsActive) setIsActive((current) => !current);
