78cf7e80e3df84b94e3ee739ea42321e79ff3ecb
kirkwood616 <kirkwood616@gmail.com>
2022-11-27 23:20:22 -0500

    Result text logic for singular or plural

--- src/admin/features/Search/SearchResults.tsx
+++ src/admin/features/Search/SearchResults.tsx
@@ -11,7 +11,9 @@ function SearchResults({ results }: Props) {
   if (results)
     return (
       <div className="SearchResults">
-        <h2>{results.length} Results</h2>
+        <h2>
+          {results.length} Result{results.length > 1 && "s"}
+        </h2>
         {results.map((result, index) => (
           <Link to={`../${result.requestStatus}/${result._id}`} key={result._id! + index} style={{ textDecoration: "none" }}>
             <InfoSection title={`${result.firstName} ${result.lastName}`} body={formatUsDate(result.requestDate)} />
