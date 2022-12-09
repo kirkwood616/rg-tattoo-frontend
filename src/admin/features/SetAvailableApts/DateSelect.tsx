1000658a18ccac2d2b3d5cb7e006ac50d5bddb15
kirkwood616 <kirkwood616@gmail.com>
2022-12-09 17:24:46 -0500

    available props, highlightDates,  autoComplete

--- src/admin/features/SetAvailableApts/DateSelect.tsx
+++ src/admin/features/SetAvailableApts/DateSelect.tsx
@@ -1,25 +1,30 @@
+import AvailableAppointments from "models/AvailableAppointments";
 import { Dispatch, SetStateAction } from "react";
 import DatePicker from "react-datepicker";
+import { populateHighlights } from "utils/DatePicker";
 import "./DateSelect.css";
 
 interface Props {
+  available: AvailableAppointments[] | undefined;
   startDate: Date | null;
   setStartDate: Dispatch<SetStateAction<Date | null>>;
 }
 
-function DateSelect({ startDate, setStartDate }: Props) {
+function DateSelect({ available, startDate, setStartDate }: Props) {
   return (
     <div className="date-picker_container">
       <span className="label">
         <label htmlFor="date-picker">Date:</label>
       </span>
       <DatePicker
+        autoComplete="off"
         name="date-picker"
         id="date-picker"
-        withPortal
         selected={startDate}
         minDate={new Date()}
+        highlightDates={populateHighlights(available)}
         onChange={(date: Date) => setStartDate(date)}
+        withPortal
       />
     </div>
   );
