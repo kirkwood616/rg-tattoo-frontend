7d7c2d992d6bbab79b82c8e7adda90e3c3bc9d03
kirkwood616 <kirkwood616@gmail.com>
2023-01-03 19:06:24 -0500

    added captcha token

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -5,8 +5,10 @@ import AvailableAppointments from "models/AvailableAppointments";
 const apiAvailableAppointments = process.env.REACT_APP_API_AVAILABLE_ROUTE_LOCAL || "";
 const apiBaseRoute = process.env.REACT_APP_API_BASE_ROUTE || "";
 
-export async function postAppointmentRequest(request: AppointmentRequest): Promise<AppointmentRequest> {
-  const res = await axios.post<AppointmentRequest>(`${apiAvailableAppointments}/new-request`, request);
+export async function postAppointmentRequest(request: AppointmentRequest, token: string): Promise<AppointmentRequest> {
+  const res = await axios.post<AppointmentRequest>(`${apiAvailableAppointments}/new-request`, request, {
+    headers: { key: token },
+  });
   return res.data;
 }
 
