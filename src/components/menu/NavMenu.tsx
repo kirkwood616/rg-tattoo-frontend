08fcd6a7474c8ffb22a50c39391fd0cbf8e55493
kirkwood616 <kirkwood616@gmail.com>
2023-01-06 15:33:11 -0500

    refactored menu

--- src/components/menu/NavMenu.tsx
+++ src/components/menu/NavMenu.tsx
@@ -2,6 +2,7 @@ import AdminMenu from "admin/components/menu/AdminMenu";
 import useAuthCheck from "hooks/useAuthCheck";
 import useLocationRoute from "hooks/useLocationRoute";
 import { useEffect, useRef, useState } from "react";
+import { toggleBooleanState } from "utils/Toggle";
 import MenuHamburger from "./MenuHamburger";
 import "./NavMenu.css";
 import UserMenu from "./UserMenu";
@@ -9,31 +10,26 @@ import UserMenu from "./UserMenu";
 function NavMenu() {
   const { user, setUser } = useAuthCheck();
   const { isAdmin, pathname } = useLocationRoute();
-
   const [isMenuActive, setIsMenuActive] = useState(false);
-
   const dropdownRef = useRef(null);
-
-  const onTriggerClick = () => setIsMenuActive((current) => !current);
 
   useEffect(() => {
     function pageClickEvent(): void {
       if (dropdownRef.current !== null) setIsMenuActive(!isMenuActive);
       else return;
     }
-
     if (isMenuActive) window.addEventListener("click", pageClickEvent);
-
     if (isMenuActive && pathname === "/user/login") setIsMenuActive((current) => !current);
-
     return () => window.removeEventListener("click", pageClickEvent);
   }, [isMenuActive, pathname]);
 
   return (
     <div className="NavMenu">
       <div className="nav-container">
-        <button onClick={onTriggerClick} className={isMenuActive ? "menu-trigger active" : "menu-trigger"}>
-          <span>MENU</span>
+        <button
+          onClick={() => toggleBooleanState(setIsMenuActive)}
+          className={isMenuActive ? "menu-trigger active" : "menu-trigger"}
+        >
           <MenuHamburger isActive={isMenuActive} />
         </button>
         <nav ref={dropdownRef} className={`menu ${isMenuActive ? "active" : "inactive"}`}>
