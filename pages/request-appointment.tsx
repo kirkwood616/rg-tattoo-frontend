71d9d68551484772682ad1fa82b3a366b9f47e8a
kirkwood616 <kirkwood616@gmail.com>
2023-01-21 16:20:19 -0500

    consolidated RequestAppointment into page

--- pages/request-appointment.tsx
+++ pages/request-appointment.tsx
@@ -1,5 +1,81 @@
-import RequestAppointment from "components/page/RequestAppointment";
+import * as Field from "components/features/request-form-fields";
+import ReviewRequest from "components/features/review-request/ReviewRequest";
+import GoButton from "components/ui/buttons/GoButton";
+import FetchError from "components/ui/errors/FetchError";
+import Loading from "components/ui/loading/Loading";
+import RequestContext from "context/RequestContext";
 import RequestContextProvider from "context/RequestContextProvider";
+import { format } from "date-fns";
+import AvailableAppointments from "models/AvailableAppointments";
+import { FormEvent, useContext, useEffect, useState } from "react";
+import { getAvailableAppointments } from "services/AppointmentService";
+import { swrOptions } from "settings/swr-options";
+import styles from "styles/pages/RequestAppointment.module.css";
+import useSWR from "swr";
+import { toggleBooleanState } from "utils/Toggle";
+
+function RequestAppointment() {
+  const [isSubmitActive, setIsSubmitActive] = useState<boolean>(false);
+  const { setAvailableAppointmentsTimes, state, dispatch } = useContext(RequestContext);
+  const { data: available, error } = useSWR<AvailableAppointments[] | undefined, Error>(
+    "available-appointments",
+    getAvailableAppointments,
+    swrOptions
+  );
+
+  useEffect(() => {
+    if (!state.startDate.value || !available) return;
+    const dateInDatabase: AvailableAppointments | undefined = available.find((appointment) => {
+      if (state.startDate.value) {
+        return appointment.date === format(state.startDate.value, "yyyy/MM/dd");
+      } else {
+        return undefined;
+      }
+    });
+    if (dateInDatabase) {
+      setAvailableAppointmentsTimes(dateInDatabase.availableTimes);
+    } else {
+      setAvailableAppointmentsTimes([]);
+    }
+  }, [state.startDate.value, available, setAvailableAppointmentsTimes]);
+
+  function onSubmit(e: FormEvent): void {
+    e.preventDefault();
+    dispatch({ type: "submitErrorCheck" });
+    if (!state.hasErrors) toggleBooleanState(setIsSubmitActive);
+  }
+
+  if (error) return <FetchError fetchError={error} />;
+  if (!available) return <Loading />;
+  return (
+    <div className={styles.RequestAppointment}>
+      <h1>REQUEST APPOINTMENT</h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <GoButton text={"DEV FILL"} onClick={() => dispatch({ type: "dev" })} />
+        <Field.SelectDate available={available} />
+        {state.startDate.value && <Field.AppointmentTimes />}
+        {state.appointmentTime.value && (
+          <>
+            <Field.FirstName />
+            <Field.LastName />
+            <Field.Age />
+            <Field.Email />
+            <Field.PhoneNumber />
+            <Field.TattooStyle />
+            <Field.TattooPlacement />
+            <Field.Budget />
+            <Field.PhotoUpload photoName="referencePhoto" />
+            <Field.PhotoUpload photoName="placementPhoto" />
+            <Field.TattooDescription />
+            <Field.RequestConfirm />
+            <GoButton type="submit" text="SUBMIT REQUEST" cssClass="button__primary" isDisabled={state.hasErrors} />
+            {isSubmitActive && <ReviewRequest setIsActive={setIsSubmitActive} />}
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
 
 export default function RequestAppointmentPage() {
   return (
